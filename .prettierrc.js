a96e34fcbcf38e4731a4bf51060d79c64c09ddbe
Przemyslaw Mejna <pmejna@gmail.com>
2022-07-09 17:32:35 +0200

    feat(github workflows): add prettier config files

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+
+// NOTE: we only override the options that are different from default
+// see https://prettier.io/docs/en/options.html
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 120,
+  };
