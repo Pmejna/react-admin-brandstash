15eb6a157368d29bd101c32db296b3395f6827fb
Przemyslaw Mejna <pmejna@gmail.com>
2022-07-10 12:01:40 +0100

    Feat(Github workflows): add unit test workflow (#26)
    Add github workflows including unit tests

---
+++ .github/pull_request_template.md
@@ -0,0 +1,13 @@
+### Description 📝
+
+_Description goes here..._
+
+### Changes ⏱️
+
+- change 1
+- change 2
+- change 3
+
+### Questions/Ideas 🙋
+
+Leave your thoughts and ideas here...
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.jest.json',
-    }
-  }
+  setupFilesAfterEnv: ['@testing-library/jest-dom', './jest.setup.ts'],
+  // jest-environment-jsdom package is installed since jsdom is not longer bundled with jest from version 28.x
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
 };
