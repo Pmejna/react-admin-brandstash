8d2d6ee1a8e8c360e9b7a68153b87120efba8393
Przemyslaw Mejna <pmejna@gmail.com>
2022-07-10 09:25:40 +0200

    feat(github workflows): add the gh workflow, add some packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.jest.json',
-    }
-  }
+  setupFilesAfterEnv: ['@testing-library/jest-dom', './jest.setup.ts'],
+  // jest-environment-jsdom package is installed since jsdom is not longer bundled with jest from version 28.x
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 };
